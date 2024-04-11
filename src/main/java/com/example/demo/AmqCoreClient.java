6a4924f9b69e7dd782eed7d546c489c6e625ddeb
Junwei <52816101+junwei567@users.noreply.github.com>
2024-04-11 17:01:25 +0800

    AMQ

--- src/main/java/com/example/demo/AmqCoreClient.java
+++ src/main/java/com/example/demo/AmqCoreClient.java
@@ -8,6 +8,8 @@ import org.springframework.jms.JmsException;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.JmsHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 
 import java.util.UUID;
 
@@ -25,37 +27,35 @@ public class AmqCoreClient implements CommandLineRunner {
     public void run(String... strings) throws Exception {
         while (true) {
             String msg = UUID.randomUUID().toString();
-            System.out.println(String.format("Sending '%s'", msg));
+            System.out.printf("Sending '%s'%n", msg);
             sendMessage(msg, null);
         }
     }
 
     public void sendMessage(String msg, Exception t) {
-        try {
+//        try {
+//            this.jmsTemplate.convertAndSend("test-queue", msg);
+//        } catch (JmsException e) {
+//            System.out.println(e.getMessage());
+//            while (e.getMessage().matches("(.*)Address(.*)is(\\s)full(.*)")) {
+//                System.err.printf("message is not sent %s", msg);
+//                try {
+//                    Thread.sleep(10000);
+//                } catch (InterruptedException ex) {
+//                    throw new RuntimeException(ex);
+//                }
+//                sendMessage(msg ,e);
+//            }
+//        }
+		new Thread(() -> {
             this.jmsTemplate.convertAndSend("test-queue", msg);
-        } catch (JmsException e) {
-            System.out.println(e.getMessage());
-            while (e.getMessage().matches("(.*)Address(.*)is(\\s)full(.*)")) {
-                System.err.printf("message is not sent %s", msg);
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException ex) {
-                    throw new RuntimeException(ex);
-                }
-                sendMessage(msg ,e);
-            }
         }
-/*		new Thread(() -> {
-            String text = UUID.randomUUID().toString();
-            System.out.println(String.format("Sending '%s'", text));
-            this.jmsTemplate.convertAndSend("test-queue", text);
-        }
-		).start();*/
+		).start();
 
     }
 
     @JmsListener(destination = "test-queue")
-    public void receiveMessage(String text) {
-        System.err.println(String.format("Received '%s'", text));
+    public void receiveMessage(String text, @Header(JmsHeaders.MESSAGE_ID) String messageId) {
+        System.out.printf("Received Message %s body: '%s'%n", messageId, text);
     }
 }
